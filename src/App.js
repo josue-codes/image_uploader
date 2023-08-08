8342c2079b422989e22434f3362884dabee8fdfd
Josue <josue@josue.codes>
2023-08-07 23:28:14 -0400

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import './App.css';
 
 import Home from "./Home";
 import LanguageSelector from "./LanguageSelector";
+import Party from "./Party"
 import { english } from "./languages";
-import { login_handler, input_text_handler,  language_handler } from "./event_handlers";
+import { input_text_handler,  language_handler } from "./event_handlers";
 
 function App() {
 
     const [language, set_language] = useState(english);
     const [party_code, set_party_code] = useState("");
     const [user_name, set_user_name] = useState("");
-    const [access_token, set_access_token] = useState(null);
     const [token_retrieval_error, set_token_retrieval_error] = useState(false);
 
     return (
         <div className="App">
-            <Home
-                language={ language }
-                party_code={ party_code }
-                party_code_handler={ input_text_handler(set_party_code) }
-                user_name={ user_name }
-                user_name_handler={ input_text_handler(set_user_name) }
-                login_handler={ login_handler(
-                    party_code,
-                    user_name,
-                    set_access_token,
-                    set_token_retrieval_error
-                ) }
-                error_message={ token_retrieval_error }
-            />
-            {/*<NavigationBar language={language} languageHandler={language_handler}/>*/}
-            {/*<FileUpload language={language} />*/}
+            <Router>
+                <Routes>
+                    <Route
+                        path='/'
+                        element={
+                            <Home
+                                language={ language }
+                                party_code={ party_code }
+                                party_code_handler={ input_text_handler(set_party_code) }
+                                user_name={ user_name }
+                                user_name_handler={ input_text_handler(set_user_name) }
+                                error_message={ token_retrieval_error }
+                                token_retrieval_error_setter={ set_token_retrieval_error }
+                            />
+                        }
+                    />
+                    <Route
+                        path='/party'
+                        element={ <Party /> }
+                    />
+                    {/*<NavigationBar language={language} languageHandler={language_handler}/>*/}
+                    {/*<FileUpload language={language} />*/}
+                </Routes>
+            </Router>
             <LanguageSelector language_handler={language_handler(set_language)}/>
         </div>
     );
