46e10de1b8ca9dbad9857088e09d8fda2b4ba76a
Josue <josue@josue.codes>
2023-08-07 23:31:17 -0400

    large refactor to get navigation handled here, login logic here and change to actual form

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 export default function Home({
     language,
@@ -6,9 +7,53 @@ export default function Home({
     party_code_handler,
     user_name,
     user_name_handler,
-    login_handler,
-    error_message
+    error_message,
+    token_retrieval_error_setter
 }) {
+    const navigate = useNavigate();
+
+    const login_handler = (
+        party_code,
+        user_name,
+        set_token_retrieval_error
+    ) => async event => {
+        event.preventDefault();
+        const url = '/api/party';
+        set_token_retrieval_error(null);
+
+        const formData = new URLSearchParams();
+        formData.append('party_code', party_code);
+        formData.append('user_name', user_name);
+
+        await fetch(url, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: formData
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw response;
+                }
+                return response.json();
+            })
+            .then(() => {
+                navigate("/party");
+            })
+            .catch(error => {
+                try {
+                    error
+                    .json()
+                    .then(message => {
+                        console.log(error);
+                        set_token_retrieval_error(message.detail.toString());
+                    });
+                } catch (e) {
+                    console.log(error);
+                    set_token_retrieval_error(error.message);
+                }
+            });
+    };
+
     return (
         <div className="home-page">
             <div className="header">
@@ -16,22 +61,30 @@ export default function Home({
                 <h2>Mobile Photo Share</h2>
             </div>
             <div className="content">
-                <h3>{ language.home_party_code }</h3>
-                <input
-                    type="text"
-                    value={ party_code }
-                    onChange={ party_code_handler }
-                />
-                <h3>{ language.home_your_name }</h3>
-                <input
-                    type="text"
-                    value={user_name}
-                    onChange={user_name_handler}
-                />
-                <div
-                    className="button"
-                    onClick={ login_handler }
-                >{ language.home_join_the_party }</div>
+                <form onSubmit={
+                    login_handler(
+                        party_code,
+                        user_name,
+                        token_retrieval_error_setter
+                    )
+                }>
+                    <h3>{ language.home_party_code }</h3>
+                    <input
+                        type="text"
+                        value={ party_code }
+                        onChange={ party_code_handler }
+                    />
+                    <h3>{ language.home_your_name }</h3>
+                    <input
+                        type="text"
+                        value={ user_name }
+                        onChange={ user_name_handler }
+                    />
+                    <button
+                        className="button"
+                        type="submit"
+                    >{ language.home_join_the_party }</button>
+                </form>
                 { error_message && <div className="error-message">{error_message}</div> }
             </div>
         </div>
