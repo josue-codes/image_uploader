33f4432312aaebbcf10386103ad3575c96bceaa1
Josue <josue@josue.codes>
2023-08-11 00:33:24 -0400

    changes to support data routers to take advantage of more react-router-dom features

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
 import './App.css';
 
@@ -16,32 +16,35 @@ function App() {
     const [user_name, set_user_name] = useState("");
     const [token_retrieval_error, set_token_retrieval_error] = useState(false);
 
+    const router = createBrowserRouter(
+        createRoutesFromElements([
+            <Route
+                path='/'
+                element={
+                    <Home
+                        language={ language }
+                        party_code={ party_code }
+                        party_code_handler={ input_text_handler(set_party_code) }
+                        user_name={ user_name }
+                        user_name_handler={ input_text_handler(set_user_name) }
+                        error_message={ token_retrieval_error }
+                        token_retrieval_error_setter={ set_token_retrieval_error }
+                    />
+                }
+            />,
+            <Route
+                path='/party'
+                element={ <Party /> }
+                loader={() => {
+                    return ['hi', 'hellow', 'how', 'are', 'you', 'today'];
+                }}
+            />
+        ])
+    );
+
     return (
         <div className="App">
-            <Router>
-                <Routes>
-                    <Route
-                        path='/'
-                        element={
-                            <Home
-                                language={ language }
-                                party_code={ party_code }
-                                party_code_handler={ input_text_handler(set_party_code) }
-                                user_name={ user_name }
-                                user_name_handler={ input_text_handler(set_user_name) }
-                                error_message={ token_retrieval_error }
-                                token_retrieval_error_setter={ set_token_retrieval_error }
-                            />
-                        }
-                    />
-                    <Route
-                        path='/party'
-                        element={ <Party /> }
-                    />
-                    {/*<NavigationBar language={language} languageHandler={language_handler}/>*/}
-                    {/*<FileUpload language={language} />*/}
-                </Routes>
-            </Router>
+            <RouterProvider router={ router }/>
             <LanguageSelector language_handler={language_handler(set_language)}/>
         </div>
     );
--- src/Party.js
+++ src/Party.js
@@ -1,18 +1,30 @@
+import { useEffect } from 'react';
+import { useNavigate, useLoaderData } from 'react-router-dom';
+
 import Cookies from 'js-cookie';
 
 
 function Party() {
     const party_code = Cookies.get('party_code');
     const user_name = Cookies.get('user_name');
+    const navigate = useNavigate();
+    const data = useLoaderData();
+
+    useEffect(() =>{
+        if (party_code === undefined || user_name === undefined) {
+            navigate('/');
+        }
+    }, []);
+
     return (
         <div className="home-page">
             <div className="header">
                 <h1>{ `Welcome to the party, ${user_name}!` }</h1>
                 <h2>{ `Let's celebrate ${party_code}!` }</h2>
             </div>
-            <div className="content">
-                <p>hi</p>
-                <p>hi</p>
+            <div className="scrollable-content">
+                <p>{data[1]}</p>
+                <p>{data[2]}</p>
                 <p>hi</p>
                 <p>hi</p>
                 <p>hi</p>
